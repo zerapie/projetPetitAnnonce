8ba7adbe98dea9d528074ba437557dfb2b429bc0
zerapie <67117189+zerapie@users.noreply.github.com>
2021-03-17 12:45:05 -0400

    1 page
    header
    footer
    index

---
+++ app/Views/common/Footer.php
@@ -0,0 +1,27 @@
+    <!-- BEGIN: Footer-->
+
+    <footer class="page-footer footer footer-static footer-light navbar-border navbar-shadow">
+        <div class="footer-copyright">
+            <div class="container"><span>&copy; 2020 <a href="http://themeforest.net/user/pixinvent/portfolio?ref=pixinvent" target="_blank">PIXINVENT</a> All rights reserved.</span><span class="right hide-on-small-only">Design and Developed by <a href="https://pixinvent.com/">PIXINVENT</a></span></div>
+        </div>
+    </footer>
+
+    <!-- END: Footer-->
+    <!-- BEGIN VENDOR JS-->
+    <script src="../../../app-assets/js/vendors.min.js"></script>
+    <!-- BEGIN VENDOR JS-->
+    <!-- BEGIN PAGE VENDOR JS-->
+    <script src="../../../app-assets/vendors/noUiSlider/nouislider.min.js"></script>
+    <!-- END PAGE VENDOR JS-->
+    <!-- BEGIN THEME  JS-->
+    <script src="../../../app-assets/js/plugins.js"></script>
+    <script src="../../../app-assets/js/search.js"></script>
+    <script src="../../../app-assets/js/custom/custom-script.js"></script>
+    <!-- END THEME  JS-->
+    <!-- BEGIN PAGE LEVEL JS-->
+    <script src="../../../app-assets/js/scripts/advance-ui-modals.js"></script>
+    <script src="../../../app-assets/js/scripts/eCommerce-products-page.js"></script>
+    <!-- END PAGE LEVEL JS-->
+</body>
+
+</html>
