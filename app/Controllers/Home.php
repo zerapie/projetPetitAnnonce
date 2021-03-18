f077cb5f173da58e2ed5a0f4e0ee8c8526c6adf7
zerapie <67117189+zerapie@users.noreply.github.com>
2021-03-17 21:11:23 -0400

    mise a jour
    header
    footer
    materialize

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,15 +6,9 @@ class Home extends BaseController
 {
 	public function index()
 	{
-		return view('welcome_message');
-	}
-	public function mapage($home = 'home')
-	{
-		echo "david";
-	}
-	public function mapage2($home = 'home', $var = 3)
-	{
-		echo $var." - perasie - ".$home;
+        echo view('common/Header');
+        echo view('index');
+        echo view('common/Footer');
 	}
 }
 
