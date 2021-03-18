ea37f79e9c11cbc535f1fd724d7da1b70d422f06
zerapie <67117189+zerapie@users.noreply.github.com>
2021-03-18 08:02:08 -0400

    mise a jour et teste 👍
    MySQL
    Register
    Login
    Filtre

---
+++ app/Controllers/Register.php
@@ -0,0 +1,62 @@
+<?php namespace App\Controllers;
+ 
+use CodeIgniter\Controller;
+use App\Models\UserModel;
+
+
+/* 
+ *Dans le contrôleur "Register.php" ci-dessus, il y a deux fonctions, à savoir : 
+ *      - la fonction index()
+ *      - la fonction save().
+ */
+
+class Register extends Controller
+{
+    /* 
+     * ----------------------------------------------------------------
+     *  Fonction index() pour afficher une vue appelée "register"
+     * ----------------------------------------------------------------
+    */
+    public function index()
+    {
+        //include helper form
+        helper(['form']);
+        $data = [];
+        echo view('register', $data);
+    }
+
+    /* 
+     * ----------------------------------------------------------------
+     *  Fonction save() enregistre les données dans la table "users" de la base de données 
+     *  ainsi que chiffrer les mots de passe avec la méthode password_hash().
+     * ----------------------------------------------------------------
+    */
+    public function save()
+    {
+        //include helper form
+        helper(['form']);
+        //set rules validation form
+        $rules = [
+            'name'          => 'required|min_length[3]|max_length[20]',
+            'email'         => 'required|min_length[6]|max_length[50]|valid_email|is_unique[users.user_email]',
+            'password'      => 'required|min_length[6]|max_length[200]',
+            'confpassword'  => 'matches[password]'
+        ];
+         
+        if($this->validate($rules)){
+            $model = new UserModel();
+            $data = [
+                'user_name'     => $this->request->getVar('name'),
+                'user_email'    => $this->request->getVar('email'),
+                'user_password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+            ];
+            $model->save($data);
+            return redirect()->to('/login');
+        }else{
+            $data['validation'] = $this->validator;
+            echo view('register', $data);
+        }
+         
+    }
+ 
+}
